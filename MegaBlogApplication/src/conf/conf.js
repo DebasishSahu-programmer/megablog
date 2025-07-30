a75a0eb2bad29eb7fe86e905d22eb0ce817f8b4e
DebasishSahu-programmer <debasishsahu5901@gmail.com>
2025-07-30 12:24:14 +0530

    remove one property

--- MegaBlogApplication/src/conf/conf.js
+++ MegaBlogApplication/src/conf/conf.js
@@ -3,8 +3,7 @@ const conf={
    appwriteProjectId:String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
    appwriteDatabaseId:String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
    appwriteCollectionId:String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-   appwriteBucketId:String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
-   appwriteTinyMCEApiKey:String(import.meta.env.VITE_APPWRITE_TINYMCE_API_KEY),
+   appwriteBucketId:String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
 }
 
 
